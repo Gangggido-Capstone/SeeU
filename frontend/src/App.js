1eca5c73103e10ca9a10f3b1a2eebf15cda288bf
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-09-08 00:30:32 +0900

    logo is only in MainPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import EyeTrackingApp from "./components/EyeTrackingApp";
-import SettingsPage from "./components/SettingsPage"; // SettingsPage 추가
-import "./App.css"; // 스타일 파일
+import SettingsPage from "./components/SettingsPage";
+import "./App.css";
 
-function App() {
+const App = () => {
     return (
         <Router>
             <div className="App">
-                <img src="/eye_logo.gif" alt="Logo" className="logo" />
+                <DisplayLogo />
                 <Routes>
-                    <Route path="/" element={<MainPage />} /> {/* 메인 페이지 */}
-                    <Route path="/eye-tracking" element={<EyeTrackingApp />} /> {/* EyeTrackingApp 페이지 */}
-                    <Route path="/settings" element={<SettingsPage />} /> {/* SettingsPage 추가 */}
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/eye-tracking" element={<EyeTrackingApp />} />
+                    <Route path="/settings" element={<SettingsPage />} />
                 </Routes>
             </div>
         </Router>
     );
-}
+};
+
+// 로고 메인 페이지만
+const DisplayLogo = () => {
+    const location = useLocation();
+    const isMainPage = location.pathname === '/';
+
+    return (
+        <>
+            {isMainPage && <img src="/eye_logo.gif" alt="Logo" className="logo" />}
+        </>
+    );
+};
 
 export default App;
