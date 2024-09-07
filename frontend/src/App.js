fcbe9c8eb44636313e04317441759eb090fbcb30
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-09-08 00:21:28 +0900

    add settings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-import "./App.css";
-import EyeTrackingApp from "./components/EyeTrackingApp.jsx";
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./components/MainPage";
+import EyeTrackingApp from "./components/EyeTrackingApp";
+import SettingsPage from "./components/SettingsPage"; // SettingsPage 추가
+import "./App.css"; // 스타일 파일
 
 function App() {
     return (
-        <div className='App'>
-            <img src="/eye_logo.gif" alt="Logo" className="logo" />
-            <EyeTrackingApp />
-        </div>
+        <Router>
+            <div className="App">
+                <img src="/eye_logo.gif" alt="Logo" className="logo" />
+                <Routes>
+                    <Route path="/" element={<MainPage />} /> {/* 메인 페이지 */}
+                    <Route path="/eye-tracking" element={<EyeTrackingApp />} /> {/* EyeTrackingApp 페이지 */}
+                    <Route path="/settings" element={<SettingsPage />} /> {/* SettingsPage 추가 */}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ frontend/src/EmptyPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EmptyPage = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>This is an empty page</h1>
+    </div>
+  );
+};
+
+export default EmptyPage;
