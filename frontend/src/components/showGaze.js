1098ae1b8e6c0a009038c0c440e42675bcf43c56
Hanjs89 <hana3269@naver.com>
2024-09-20 20:59:55 +0900

    modified files

--- frontend/src/components/showGaze.js
+++ frontend/src/components/showGaze.js
@@ -27,10 +27,10 @@ function showGazeDotOnDom(gazeInfo) {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
         let ctx = canvas.getContext("2d");
-        ctx.fillStyle = "#FF0000";
+        ctx.fillStyle = "rgba(250, 250, 250, 0.5)";
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
-        ctx.arc(gazeInfo.x, gazeInfo.y, 10, 0, Math.PI * 2, true);
+        ctx.arc(gazeInfo.x, gazeInfo.y, 20, 0, Math.PI * 2, true);
         ctx.fill();
     }
 }
