feb370862ccdfb32a611fbae7db021c26171ab93
Hanjs89 <hana3269@naver.com>
2024-10-05 00:32:18 +0900

    Save Attention Data

--- backend/src/main/java/com/han/youtube/controller/GazeDataController.java
+++ backend/src/main/java/com/han/youtube/controller/GazeDataController.java
@@ -1,8 +1,10 @@
 package com.han.youtube.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
@@ -12,30 +14,48 @@ import java.util.Map;
 @CrossOrigin(origins = "http://localhost:9000")
 public class GazeDataController {
 
-    // 시선 데이터 저장 요청을 처리하는 엔드포인트
     @PostMapping("api/save-gaze-data")
-    public ResponseEntity<String> saveGazeData(@RequestBody List<Map<String, Object>> gazeRecords) {
-        String filePath = "C:/youtube-seeso-demo/Data/GazeData/gaze_data.csv"; // 원하는 경로로 변경 가능
+    public ResponseEntity<String> saveGazeData(@RequestBody Map<String, Object> payload) {
+        String videoId = (String) payload.get("videoId");
+        String watchDate = (String) payload.get("watchDate");
+        
+        // 비디오 크기 값 videoFrame.get("width"), videoFrame.get("height")
+        Map<String, Object> videoFrame = null;
+        if (payload.get("videoFrame") instanceof Map) {
+            videoFrame = (Map<String, Object>) payload.get("videoFrame");
+        }
 
-        try (FileWriter writer = new FileWriter(filePath)) {
-            // CSV 파일 헤더
-            writer.append("Time,X,Y\n");
+        // 시선 데이터
+        List<Map<String, Object>> gazeData = null;
+        if (payload.get("gazeData") instanceof List) {
+            gazeData = (List<Map<String, Object>>) payload.get("gazeData");
+        }
+
+        // CSV 파일 경로 설정
+        String filePath = "C:/youtube-seeso-demo/Data/GazeData/" + videoId + "_" + watchDate.replace(":", "-") + ".csv";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            // 헤더
+            writer.append("Time,X,Y,Attention\n");
 
             // 시선 좌표 데이터를 CSV 파일에 작성
-            for (Map<String, Object> record : gazeRecords) {
-                writer.append(record.get("time").toString())
+            for (Map<String, Object> record : gazeData) {
+                writer.append(record.get("time") != null ? record.get("time").toString() : "null")
                         .append(",")
-                        .append(record.get("x").toString())
+                        .append(record.get("x") != null ? record.get("x").toString() : "null")
                         .append(",")
-                        .append(record.get("y").toString())
+                        .append(record.get("y") != null ? record.get("y").toString() : "null")
+                        .append(",")
+                        .append(record.get("attention") != null ? record.get("attention").toString() : "null")
                         .append("\n");
             }
 
-            writer.flush(); // 파일 작성 완료
-            return ResponseEntity.ok("파일 저장 성공: " + filePath);
+            writer.flush();  // 파일에 데이터 저장
+
+            return new ResponseEntity<>("CSV 파일이 성공적으로 저장되었습니다.", HttpStatus.OK);
         } catch (IOException e) {
             e.printStackTrace();
-            return ResponseEntity.status(500).body("파일 저장 실패");
+            return new ResponseEntity<>("CSV 파일 저장 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- frontend/src/components/VideoGazeTracker.js
+++ frontend/src/components/VideoGazeTracker.js
@@ -11,15 +11,23 @@ const VideoGazeTracker = () => {
     const [isApiReady, setIsApiReady] = useState(false); // YouTube API 로드 상태를 저장하는 상태
     const [startTracking, setStartTracking] = useState(() => {}); // seeso 시선 추적 시작
     const [stopTracking, setStopTracking] = useState(() => {}); // seeso 시선 추적 정지
-    const [gazeData, setGazeData] = useState({ x: NaN, y: NaN }); // 시선 좌표
-    const [videoGaze, setVideoGaze] = useState({ x: NaN, y: NaN }); // 교정된 시선 좌표
+    const [gazeData, setGazeData] = useState({
+        x: NaN,
+        y: NaN,
+        attention: 3,
+    }); // 시선 좌표
+    const [videoGaze, setVideoGaze] = useState({
+        x: NaN,
+        y: NaN,
+        attention: 3,
+    }); // 교정된 시선 좌표
     const [videoFrame, setVideoFrame] = useState({
         top: 0,
         left: 0,
         height: 0,
         width: 0,
     }); // 영상 위치와 크기
-    const [gazeRecords, setGazeRecords] = useState([]); // 시선 좌표와 시간을 저장할 배열
+    const [videoGazeData, setVideoGazeData] = useState([]); // 시선 좌표와 시간을 저장할 배열
 
     useEffect(() => {
         // YouTube IFrame Player API가 로드되었을 때 호출되는 함수
@@ -86,11 +94,11 @@ const VideoGazeTracker = () => {
         if (event.data === window.YT.PlayerState.PLAYING) {
             // 동영상이 재생 중일 때
             setIsPlaying(true); // 재생 상태를 true로 설정
-            // 1초마다 현재 재생 시간을 가져와 상태에 저장
+
             const interval = setInterval(() => {
                 const time = event.target.getCurrentTime(); // 현재 재생 시간
-                setCurrentTime(Math.floor(time * 2) / 2); // 0.5초 단위로 반올림
-            }, 500);
+                setCurrentTime(time);
+            }, 300);
 
             return () => clearInterval(interval); // 정리 함수로 인터벌을 해제
         } else {
@@ -100,27 +108,36 @@ const VideoGazeTracker = () => {
 
     // 교정된 시선 좌표 및 시간 기록 로직
     useEffect(() => {
-        if (!isNaN(gazeData.x) && !isNaN(gazeData.y)) {
-            const videoX = gazeData.x - videoFrame.left;
-            const videoY = gazeData.y - videoFrame.top;
-            setVideoGaze({ x: videoX, y: videoY });
+        let videoX = gazeData.x - videoFrame.left;
+        let videoY = gazeData.y - videoFrame.top;
+        let attention = gazeData.eyemovementState;
 
-            if (isPlaying) {
-                // 현재 시간에 해당하는 좌표가 이미 기록되어 있지 않은 경우만 저장
-                const alreadyRecorded = gazeRecords.some(
-                    (record) => record.time === currentTime
+        setVideoGaze({
+            x: videoX,
+            y: videoY,
+            attention: attention,
+        });
+
+        if (isPlaying) {
+            // 현재 시간에 해당하는 좌표가 이미 기록되어 있지 않은 경우만 저장
+            const alreadyRecorded = videoGazeData.some(
+                (record) => record.time === currentTime
+            );
+            if (!alreadyRecorded) {
+                setVideoGazeData((prevRecords) => [
+                    ...prevRecords,
+                    {
+                        time: currentTime,
+                        x: videoX,
+                        y: videoY,
+                        attention: attention,
+                    },
+                ]);
+
+                // 저장 데이터 확인용 로그
+                console.log(
+                    `Time: ${currentTime}, X: ${videoX}, Y: ${videoY}, , attention: ${attention}`
                 );
-                if (!alreadyRecorded) {
-                    setGazeRecords((prevRecords) => [
-                        ...prevRecords,
-                        { time: currentTime, x: videoX, y: videoY },
-                    ]);
-
-                    // 저장 데이터 확인용 로그
-                    console.log(
-                        `Time: ${currentTime}, X: ${videoX}, Y: ${videoY}`
-                    );
-                }
             }
         }
     }, [gazeData, videoFrame, currentTime, isPlaying]);
@@ -153,9 +170,17 @@ const VideoGazeTracker = () => {
     };
 
     // api
-    // + 영상 ID, 영상 크기(W,H), 시청 날짜(시간) 세부정보도 같이 전달 코드 추가하기
+    // + 영상 ID, 영상 크기(W,H), 시청 날짜와 현재 시간 세부정보도 같이 전달 코드 추가하기
     const saveCSVToServer = async () => {
         try {
+            // 전송할 데이터 구성
+            const dataToSend = {
+                videoId: videoId, // 영상 ID
+                videoFrame: videoFrame, // 영상 위치 및 크기
+                watchDate: new Date().toISOString(), // 시청 날짜와 현재 시간 (ISO 8601 형식)
+                gazeData: videoGazeData, // 시선 좌표 데이터
+            };
+
             const response = await fetch(
                 "http://localhost:8080/api/save-gaze-data",
                 {
@@ -163,14 +188,18 @@ const VideoGazeTracker = () => {
                     headers: {
                         "Content-Type": "application/json",
                     },
-                    body: JSON.stringify(gazeRecords), // 시선 좌표 데이터를 서버로 전송
+                    body: JSON.stringify(dataToSend), // JSON 형태로 서버에 전송
                 }
             );
 
+            const result = await response.text();
+
             if (response.ok) {
-                console.log("파일이 서버에 저장되었습니다.");
+                console.log("파일이 서버에 성공적으로 저장되었습니다.");
+                console.log(result); // 성공 메시지 출력
             } else {
                 console.error("서버에 파일 저장 실패");
+                console.error(result); // 실패 메시지 출력
             }
         } catch (error) {
             console.error("서버 요청 중 오류 발생:", error);
@@ -221,9 +250,10 @@ const VideoGazeTracker = () => {
                 {/* <p>
                     시선 좌표: x: {gazeData.x}, y: {gazeData.y}
                 </p> */}
-                {/* <p>
-                    교정된 시선 좌표: x: {videoGaze.x}, y: {videoGaze.y}
-                </p> */}
+                <p>
+                    교정된 시선 좌표: x: {videoGaze.x}, y: {videoGaze.y},
+                    attention: {videoGaze.attention}
+                </p>
                 {/* 영상 재생 시간 및 좌표 */}
                 {/* <p>현재 재생 시간: {Math.floor(currentTime)}초</p>
                 <p>
