38fec2e46ea8640b8f3cbc06f31098a6244e626a
Hanjs89 <hana3269@naver.com>
2024-10-05 14:52:38 +0900

    Refactor Move business logic to service layer

--- backend/src/main/java/com/han/youtube/controller/GazeDataController.java
+++ backend/src/main/java/com/han/youtube/controller/GazeDataController.java
@@ -1,59 +1,30 @@
 package com.han.youtube.controller;
 
+import com.han.youtube.service.GazeDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:9000")
+@RequestMapping("/api")
 public class GazeDataController {
 
-    @PostMapping("api/save-gaze-data")
+    private final GazeDataService gazeDataService;
+
+    @Autowired
+    public GazeDataController(GazeDataService gazeDataService) {
+        this.gazeDataService = gazeDataService;
+    }
+
+    @PostMapping("/save-gaze-data")
     public ResponseEntity<String> saveGazeData(@RequestBody Map<String, Object> payload) {
-        String videoId = (String) payload.get("videoId");
-        String watchDate = (String) payload.get("watchDate");
-        
-        // 비디오 크기 값 videoFrame.get("width"), videoFrame.get("height")
-        Map<String, Object> videoFrame = null;
-        if (payload.get("videoFrame") instanceof Map) {
-            videoFrame = (Map<String, Object>) payload.get("videoFrame");
-        }
-
-        // 시선 데이터
-        List<Map<String, Object>> gazeData = null;
-        if (payload.get("gazeData") instanceof List) {
-            gazeData = (List<Map<String, Object>>) payload.get("gazeData");
-        }
-
-        // CSV 파일 경로 설정
-        String filePath = "C:/youtube-seeso-demo/Data/GazeData/" + videoId + "_" + watchDate.replace(":", "-") + ".csv";
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            // 헤더
-            writer.append("Time,X,Y,Attention\n");
-
-            // 시선 좌표 데이터를 CSV 파일에 작성
-            for (Map<String, Object> record : gazeData) {
-                writer.append(record.get("time") != null ? record.get("time").toString() : "null")
-                        .append(",")
-                        .append(record.get("x") != null ? record.get("x").toString() : "null")
-                        .append(",")
-                        .append(record.get("y") != null ? record.get("y").toString() : "null")
-                        .append(",")
-                        .append(record.get("attention") != null ? record.get("attention").toString() : "null")
-                        .append("\n");
-            }
-
-            writer.flush();  // 파일에 데이터 저장
-
+        try {
+            gazeDataService.saveGazeData(payload);
             return new ResponseEntity<>("CSV 파일이 성공적으로 저장되었습니다.", HttpStatus.OK);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>("CSV 파일 저장 중 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
---
+++ backend/src/main/java/com/han/youtube/service/GazeDataService.java
@@ -0,0 +1,51 @@
+package com.han.youtube.service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class GazeDataService {
+    public void saveGazeData(Map<String, Object> payload) throws IOException {
+        String videoId = (String) payload.get("videoId");
+        String watchDate = (String) payload.get("watchDate");
+
+        // 비디오 크기 값 videoFrame.get("width"), videoFrame.get("height")
+        Map<String, Object> videoFrame = null;
+        if (payload.get("videoFrame") instanceof Map) {
+            videoFrame = (Map<String, Object>) payload.get("videoFrame");
+        }
+
+        // 시선 데이터
+        List<Map<String, Object>> gazeData = null;
+        if (payload.get("gazeData") instanceof List) {
+            gazeData = (List<Map<String, Object>>) payload.get("gazeData");
+        }
+
+        // CSV 파일 경로 설정
+        String filePath = "C:/youtube-seeso-demo/Data/GazeData/" + videoId + "_" + watchDate.replace(":", "-") + ".csv";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            // 헤더
+            writer.append("Time,X,Y,Attention\n");
+
+            // 시선 좌표 데이터를 CSV 파일에 작성
+            for (Map<String, Object> record : gazeData) {
+                writer.append(record.get("time") != null ? record.get("time").toString() : "null")
+                        .append(",")
+                        .append(record.get("x") != null ? record.get("x").toString() : "null")
+                        .append(",")
+                        .append(record.get("y") != null ? record.get("y").toString() : "null")
+                        .append(",")
+                        .append(record.get("attention") != null ? record.get("attention").toString() : "null")
+                        .append("\n");
+            }
+
+            writer.flush();  // 파일에 데이터 저장
+        }
+    }
+}
