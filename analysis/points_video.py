0e30f847cc253015b7188f30f1f61c7e1ee22040
Hanjs89 <hana3269@naver.com>
2024-10-12 23:43:27 +0900

    code test

--- analysis/points_video.py
+++ analysis/points_video.py
@@ -1,140 +1,112 @@
 import os
-import yt_dlp
 import subprocess
 import cv2
 import pandas as pd
-from datetime import datetime
+from video_download import download
 
 # CSV 파일 경로
-csv_file = "Data\\GazeData\\0gkPFSvVvFw_2024-10-12-16-50-03.csv"
-dt = "2024-10-12-16-50-03"
+csv_file = "Data\\GazeData\\0gkPFSvVvFw_2024-10-12-18-56-44.csv"
+dt = "2024-10-12-18-56-44"
 
 # CSV 데이터 로드
 df = pd.read_csv(csv_file)
 
-# 1. 유튜브 영상 다운로드
-# timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
 video_id = "0gkPFSvVvFw"
-video_url = f"https://youtu.be/{video_id}"  # 유튜브 영상 URL
-video_only = f"analysis/input_video/temp/{video_id}_video_only.mp4"  # 비디오 파일 경로
-audio_only = f"analysis/input_video/temp/{video_id}_audio_only.m4a"  # 오디오 파일 경로
+
+# 유튜브 영상 다운로드
+video_only, audio_only, video_filename = download(video_id)
+
+os.makedirs(f"analysis/video/{video_id}/points", exist_ok=True)
+point_video = f"analysis/video/{video_id}/points/{video_id}_{dt}.mp4"
+
+if not os.path.exists(point_video):
+    # 비디오 열기
+    cap = cv2.VideoCapture(video_only)
+
+    # 비디오 속성 가져오기
+    fps = cap.get(cv2.CAP_PROP_FPS)  # 초당 프레임 수
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 비디오 너비
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 비디오 높이
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # 총 프레임 수
+
+    # 비디오를 1207x679 크기로 변환 (노트북: 965X543)
+    output_width = 965
+    output_height = 543
+
+    # 비디오 출력 설정
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    video_with_points = f"analysis/video/{video_id}/points.mp4"
+    out = cv2.VideoWriter(video_with_points, fourcc, fps, (output_width, output_height))
 
 
-# yt-dlp 옵션 설정: 비디오와 오디오를 별도로 다운로드
-ydl_opts_video = {
-    'format': 'bestvideo',
-    'outtmpl': video_only,
-}
+    # 현재 프레임 인덱스
+    frame_idx = 0
 
-ydl_opts_audio = {
-    'format': 'bestaudio',
-    'outtmpl': audio_only,
-}
+    # Time 간격을 프레임 수로 변환 (0.1초 간격)
+    time_interval = 0.1
+    frames_per_interval = int(time_interval * fps)
 
-# 비디오 다운로드
-with yt_dlp.YoutubeDL(ydl_opts_video) as ydl:
-    ydl.download([video_url])
+    # 비디오를 프레임 단위로 처리
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-# 오디오 다운로드
-with yt_dlp.YoutubeDL(ydl_opts_audio) as ydl:
-    ydl.download([video_url])
+        # 현재 프레임에 해당하는 시간 계산 (초 단위)
+        current_time = frame_idx / fps
+        
+        # 프레임 크기 조정
+        frame_resized = cv2.resize(frame, (output_width, output_height))
+        
+        # Time 값에 해당하는 데이터 가져오기
+        matching_rows = df[(df['Time'] >= current_time) & (df['Time'] < current_time + time_interval)]
+        
+        # 각 행에 대해 X, Y 좌표에 점 그리기 (null 값 무시)
+        for _, row in matching_rows.iterrows():
+            x, y = row['X'], row['Y']
+            if not pd.isna(x) and not pd.isna(y):
+                # X, Y 좌표에 점 그리기 (파란색 점, 반지름 5)
+                print("Time: "+ str(row['Time']) + ", " + "X: "+ str(int(x)) + ", " + "Y: "+ str(int(y)))
+                cv2.circle(frame_resized, (int(x), int(y)), 5, (250, 0, 0), -1)
+        
+        # 결과 프레임을 비디오에 기록
+        out.write(frame_resized)
+        
+        # 다음 프레임으로 이동
+        frame_idx += 1
 
-# 비디오 열기
-cap = cv2.VideoCapture(video_only)
+    # 비디오와 출력 파일 닫기
+    cap.release()
+    out.release()
 
-# 비디오 속성 가져오기
-fps = cap.get(cv2.CAP_PROP_FPS)  # 초당 프레임 수
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))  # 비디오 너비
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 비디오 높이
-frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # 총 프레임 수
+    #  ==========================================================  #
 
-# 비디오를 1207x679 크기로 변환 (노트북: 965X543)
-output_width = 965
-output_height = 543
+    # ffmpeg를 사용해 비디오와 오디오 병합
+    merge_command = [
+        'ffmpeg',
+        '-i', video_with_points,         # 좌표가 그려진 비디오 파일
+        '-i', audio_only,         # 입력 오디오 파일
+        '-c:v', 'libx264',        # 비디오를 H.264로 재인코딩
+        '-preset', 'fast',        # 빠른 인코딩 옵션
+        '-c:a', 'aac',            # 오디오를 AAC 형식으로 변환
+        '-b:a', '192k',           # 오디오 비트레이트 설정
+        '-r', '30',               # 비디오 프레임 속도 강제 설정 (30fps, 고정 프레임 속도)
+        '-vsync', 'cfr',          # 고정 프레임 속도 동기화
+        '-map', '0:v:0',          # 비디오 스트림을 첫 번째 입력에서 가져오기
+        '-map', '1:a:0',          # 오디오 스트림을 두 번째 입력에서 가져오기
+        '-shortest',              # 가장 짧은 스트림 기준으로 맞추기
+        point_video                 # 최종 출력 파일 경로
+    ]
 
-# 비디오 출력 설정
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-video_with_points = f"analysis/input_video/temp/{video_id}_points.mp4"
-out = cv2.VideoWriter(video_with_points, fourcc, fps, (output_width, output_height))
+    # 병합 명령어 실행
+    subprocess.run(merge_command)
 
+    # 병합 완료 후 비디오 및 오디오 파일 삭제
+    if os.path.exists(video_with_points):
+        os.remove(video_with_points)
+        print(f"{video_with_points} 파일이 삭제되었습니다.")
 
-# 현재 프레임 인덱스
-frame_idx = 0
+    print(f"{point_video} 비디오 생성 완료")
 
-# Time 간격을 프레임 수로 변환 (0.3초 간격)
-time_interval = 0.3
-frames_per_interval = int(time_interval * fps)
-
-# 비디오를 프레임 단위로 처리
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
-
-    # 현재 프레임에 해당하는 시간 계산 (초 단위)
-    current_time = frame_idx / fps
-    
-    # 프레임 크기 조정
-    frame_resized = cv2.resize(frame, (output_width, output_height))
-    
-    # Time 값에 해당하는 데이터 가져오기
-    matching_rows = df[(df['Time'] >= current_time) & (df['Time'] < current_time + time_interval)]
-    
-    # 각 행에 대해 X, Y 좌표에 점 그리기 (null 값 무시)
-    for _, row in matching_rows.iterrows():
-        x, y = row['X'], row['Y']
-        if not pd.isna(x) and not pd.isna(y):
-            # X, Y 좌표에 점 그리기 (파란색 점, 반지름 5)
-            print(x, y)
-            cv2.circle(frame_resized, (int(x), int(y)), 5, (255, 0, 0), -1)
-    
-    # 결과 프레임을 비디오에 기록
-    out.write(frame_resized)
-    
-    # 다음 프레임으로 이동
-    frame_idx += 1
-
-# 비디오와 출력 파일 닫기
-cap.release()
-out.release()
-
-
-#  ==========================================================  #
-os.makedirs(f"analysis/{video_id}/points", exist_ok=True)
-out_video = f"analysis/{video_id}/points/{video_id}_{dt}.mp4"
-
-# ffmpeg를 사용해 비디오와 오디오 병합
-merge_command = [
-    'ffmpeg',
-    '-i', video_with_points,         # 좌표가 그려진 비디오 파일
-    '-i', audio_only,         # 입력 오디오 파일
-    '-c:v', 'libx264',        # 비디오를 H.264로 재인코딩
-    '-preset', 'fast',        # 빠른 인코딩 옵션
-    '-c:a', 'aac',            # 오디오를 AAC 형식으로 변환
-    '-b:a', '192k',           # 오디오 비트레이트 설정
-    '-r', '30',               # 비디오 프레임 속도 강제 설정 (30fps, 고정 프레임 속도)
-    '-vsync', 'cfr',          # 고정 프레임 속도 동기화
-    '-map', '0:v:0',          # 비디오 스트림을 첫 번째 입력에서 가져오기
-    '-map', '1:a:0',          # 오디오 스트림을 두 번째 입력에서 가져오기
-    '-shortest',              # 가장 짧은 스트림 기준으로 맞추기
-    out_video                 # 최종 출력 파일 경로
-]
-
-# 병합 명령어 실행
-subprocess.run(merge_command)
-
-# 병합 완료 후 비디오 및 오디오 파일 삭제
-if os.path.exists(video_only):
-    os.remove(video_only)
-    print(f"{video_only} 파일이 삭제되었습니다.")
-
-if os.path.exists(audio_only):
-    os.remove(audio_only)
-    print(f"{audio_only} 파일이 삭제되었습니다.")
-
-if os.path.exists(video_with_points):
-    os.remove(video_with_points)
-    print(f"{video_with_points} 파일이 삭제되었습니다.")
-
-
-print("좌표가 그려진 비디오와 오디오 병합 완료 및 파일 삭제")
+else:
+        print(f"{point_video} 이미 존재합니다.")
---
+++ analysis/pysceneDetect_test.py
@@ -0,0 +1,66 @@
+from scenedetect import open_video, SceneManager
+from scenedetect.detectors import ContentDetector
+from scenedetect.scene_manager import save_images
+from scenedetect.video_splitter import split_video_ffmpeg
+from video_download import download
+import os
+import cv2
+# "HdOhm3v4Sg8"
+# 0gkPFSvVvFw 전란
+video_id = "0gkPFSvVvFw"
+
+# 유튜브 영상 다운로드
+video_only, audio_only, video_filename = download(video_id)
+
+# 파일이 이미 존재하는지 확인
+split_video_directory = f"analysis/video/{video_id}/split_video"
+
+if not os.path.exists(split_video_directory):
+    # 영상 불러오기
+    video = open_video(video_filename)
+
+    # 초당 프레임 수
+    fps = cv2.VideoCapture(video_only).get(cv2.CAP_PROP_FPS)  
+    f = fps * 8 # 장면 당 최소 프레임 수
+    # 디텍터 생성, 임계값 35, 장면 당 최소 프레임 수 
+    content_detector = ContentDetector(threshold=35, min_scene_len=f)
+
+    # Scene Manager 생성
+    scene_manager = SceneManager()
+    scene_manager.add_detector(content_detector)
+
+    # detect 수행 (영상의 처음부터 끝까지 detect)
+    scene_manager.detect_scenes(video, show_progress=True)
+
+    # `get_scene_list` 리스트의 시작과 끝 timecode pairs 을 리턴
+    scene_list = scene_manager.get_scene_list()
+
+    #장면 시작하는 시간을 저장하는 리스트
+    sceneTime = [] 
+
+    # 장면 분할 결과 출력
+    for scene in scene_list:
+        start, end = scene
+        if start.get_seconds()== 0:
+            sceneTime.append(round(start.get_seconds(), 4))
+        else:
+            sceneTime.append(round(start.get_seconds(), 4) - 0.3)
+
+        print(start, "~", end)   
+    print()    
+    print(sceneTime)
+
+    # 영상 자르기 (파일로 저장)
+    split_video_ffmpeg(video_filename, scene_list, output_dir=split_video_directory,show_progress=True)
+
+    # 썸네일 만들기 (jpg 파일로 저장)
+    save_images(
+        scene_list, # 장면 리스트 [(시작, 끝)]
+        video, # 영상
+        num_images=1, # 각 장면 당 이미지 개수
+        image_name_template=f'{video_id}_$SCENE_NUMBER', # 결과 이미지 파일 이름
+        output_dir=f'analysis/video/{video_id}/thumbnails') # 결과 디렉토리 이름
+
+    print(f"{video_id} 영상 분할 완료")
+else:
+    print(f"{video_id} split_video가 이미 존재합니다. 다운로드를 건너뜁니다.")
