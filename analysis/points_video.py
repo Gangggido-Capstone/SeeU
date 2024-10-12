f8760b11c06e11f22afa9d7dc8d9213edbb0f0d4
Hanjs89 <hana3269@naver.com>
2024-10-12 17:42:53 +0900

    rename

--- analysis/points_video.py
+++ analysis/points_video.py
@@ -6,18 +6,19 @@ import pandas as pd
 from datetime import datetime
 
 # CSV 파일 경로
-csv_file = "Data\\GazeData\\RoER-ab1QYw_2024-10-09-20-55-37.csv"
+csv_file = "Data\\GazeData\\0gkPFSvVvFw_2024-10-12-16-50-03.csv"
+dt = "2024-10-12-16-50-03"
 
 # CSV 데이터 로드
 df = pd.read_csv(csv_file)
 
 # 1. 유튜브 영상 다운로드
-timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-video_id = "RoER-ab1QYw"
+# timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+video_id = "0gkPFSvVvFw"
 video_url = f"https://youtu.be/{video_id}"  # 유튜브 영상 URL
-video_filename = f"analysis/input_video/point_{video_id}_{timestamp}.mp4"  # 최종 저장할 비디오 파일 경로
-video_only = f"analysis/input_video/video_only_{timestamp}.mp4"  # 비디오 파일 경로
-audio_only = f"analysis/input_video/audio_only_{timestamp}.m4a"  # 오디오 파일 경로
+video_only = f"analysis/input_video/temp/{video_id}_video_only.mp4"  # 비디오 파일 경로
+audio_only = f"analysis/input_video/temp/{video_id}_audio_only.m4a"  # 오디오 파일 경로
+
 
 # yt-dlp 옵션 설정: 비디오와 오디오를 별도로 다운로드
 ydl_opts_video = {
@@ -53,8 +54,9 @@ output_height = 543
 
 # 비디오 출력 설정
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-video_with_points = f"analysis/input_video/video_with_points_{timestamp}.mp4"
+video_with_points = f"analysis/input_video/temp/{video_id}_points.mp4"
 out = cv2.VideoWriter(video_with_points, fourcc, fps, (output_width, output_height))
+
 
 # 현재 프레임 인덱스
 frame_idx = 0
@@ -72,7 +74,7 @@ while cap.isOpened():
     # 현재 프레임에 해당하는 시간 계산 (초 단위)
     current_time = frame_idx / fps
     
-    # 프레임 크기를 1207x679로 조정
+    # 프레임 크기 조정
     frame_resized = cv2.resize(frame, (output_width, output_height))
     
     # Time 값에 해당하는 데이터 가져오기
@@ -83,6 +85,7 @@ while cap.isOpened():
         x, y = row['X'], row['Y']
         if not pd.isna(x) and not pd.isna(y):
             # X, Y 좌표에 점 그리기 (파란색 점, 반지름 5)
+            print(x, y)
             cv2.circle(frame_resized, (int(x), int(y)), 5, (255, 0, 0), -1)
     
     # 결과 프레임을 비디오에 기록
@@ -95,10 +98,15 @@ while cap.isOpened():
 cap.release()
 out.release()
 
+
+#  ==========================================================  #
+os.makedirs(f"analysis/{video_id}/points", exist_ok=True)
+out_video = f"analysis/{video_id}/points/{video_id}_{dt}.mp4"
+
 # ffmpeg를 사용해 비디오와 오디오 병합
 merge_command = [
     'ffmpeg',
-    '-i', video_with_points,  # 좌표가 그려진 비디오 파일
+    '-i', video_with_points,         # 좌표가 그려진 비디오 파일
     '-i', audio_only,         # 입력 오디오 파일
     '-c:v', 'libx264',        # 비디오를 H.264로 재인코딩
     '-preset', 'fast',        # 빠른 인코딩 옵션
@@ -109,7 +117,7 @@ merge_command = [
     '-map', '0:v:0',          # 비디오 스트림을 첫 번째 입력에서 가져오기
     '-map', '1:a:0',          # 오디오 스트림을 두 번째 입력에서 가져오기
     '-shortest',              # 가장 짧은 스트림 기준으로 맞추기
-    video_filename            # 최종 출력 파일 경로
+    out_video                 # 최종 출력 파일 경로
 ]
 
 # 병합 명령어 실행
@@ -127,5 +135,6 @@ if os.path.exists(audio_only):
 if os.path.exists(video_with_points):
     os.remove(video_with_points)
     print(f"{video_with_points} 파일이 삭제되었습니다.")
+
 
 print("좌표가 그려진 비디오와 오디오 병합 완료 및 파일 삭제")
