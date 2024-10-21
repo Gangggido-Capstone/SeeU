a89ee2b6a6192c4fa7a0863c8bc7b74b9d2111cc
JunSang1121 <juns1021@naver.com>
2024-10-22 01:38:20 +0900

    video_analysis 인자 넘겨주는 값 추가

--- analysis/video_analysis.py
+++ analysis/video_analysis.py
@@ -1,3 +1,4 @@
+import sys
 from video_download import download
 from video_detect import detect
 from video_gaze_visualization import gazeVisualization
@@ -8,9 +9,20 @@ from video_score_cluster import score_cluster
 # UrEHWclh7Co 삼성카드
 # 0gkPFSvVvFw 전란
 # fRaIcUhaXXQ 핫초코
+
 video_id = "0gkPFSvVvFw"
 video_csv = "0gkPFSvVvFw_2024-10-12-18-56-44.csv"
+
 # =====================================================
+
+
+#if len(sys.argv) != 3:
+#    print("전달된 인수 초과")
+#    sys.exit(1)
+
+#video_id = sys.argv[1]  # 첫 번째 인자는 video_id
+#video_csv = sys.argv[2]  # 두 번째 인자는 video_csv
+
 
 # 영상 다운
 video_only, audio_only, video_filename = download(video_id)
