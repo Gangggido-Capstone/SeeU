0247e643090129f00a29f932d4ba34663590f318
Hanjs89 <hana3269@naver.com>
2024-10-21 22:57:21 +0900

    Update to existing code function

--- analysis/video_analysis.py
+++ analysis/video_analysis.py
@@ -2,31 +2,48 @@ from video_download import download
 from video_detect import detect
 from video_gaze_visualization import gazeVisualization
 from video_score_cluster import score_cluster
+import sys
+import json
+
+def main():
+    # 영상 다운
+    print("영상 다운 시작")
+    video_only, audio_only, video_filename = download(video_id)
+    print("영상 다운 끝")
+
+    # 영상 분할
+    print("영상 분할 시작")
+    sceneTime = detect(video_id, video_only, video_filename)
+    print("영상 분할 끝")
+
+    # 영상 점수 및 클러스터 이미지 저장
+    print("영상 점수 및 클러스터 이미지 저장 시작")
+    attention_score_list = score_cluster(video_id, video_csv, sceneTime)
+    print("영상 점수 및 클러스터 이미지 저장 끝")
+
+    # 영상 분석 시각화
+    print("영상 분석 시각화 시작")
+    video_point = gazeVisualization(video_id, video_csv, video_only, audio_only)
+    print("영상 분석 시각화 끝")
 
 
-# video_id, video_csv는 스프링에서 넘겨야 함 ============
-# UrEHWclh7Co 삼성카드
-# 0gkPFSvVvFw 전란
-# fRaIcUhaXXQ 핫초코
-video_id = "0gkPFSvVvFw"
-video_csv = "0gkPFSvVvFw_2024-10-12-18-56-44.csv"
-# =====================================================
+    # 최종적으로 반환할 값은 JSON 형식으로 출력
+    print("최종적으로 반환할 값은 JSON 형식으로 출력 시작")
+    result = {
+        "attention_score_list": attention_score_list,
+        "video_point": video_point
+    }
+    
+    # 마지막에 JSON 출력
+    print(json.dumps(result))
 
-# 영상 다운
-video_only, audio_only, video_filename = download(video_id)
-
-# 영상 분할
-sceneTime = detect(video_id, video_only, video_filename)
-
-# 영상 점수 및 클러스터 이미지 저장
-atention_score_list = score_cluster(video_id, video_csv, sceneTime)
-
-# 영상 분석 시각화
-video_point = gazeVisualization(video_id, video_csv, video_only, audio_only)
-print(video_point)
-
-
-# 아래 값 스프링에 전달해야 함 ================
-for atention_score in atention_score_list:
-    print(atention_score)
-print(video_point)
+if __name__ == "__main__":
+    # video_id, video_csv는 스프링에서 넘겨야 함 ============
+    # UrEHWclh7Co 삼성카드
+    # 0gkPFSvVvFw 전란
+    # fRaIcUhaXXQ 핫초코
+    video_id = "0gkPFSvVvFw"
+    video_csv = "0gkPFSvVvFw_2024-10-12-18-56-44.csv"
+    # =====================================================
+    
+    main()
