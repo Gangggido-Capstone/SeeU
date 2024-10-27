12ed755f7210e6194b68fc32f51ece1306c3d3b5
Yoonji Lee <rhaxlddbswl3@gmail.com>
2024-10-28 02:55:44 +0900

    feat: analysis modal

--- frontend/src/components/RecordPage.js
+++ frontend/src/components/RecordPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../../css/RecordPage.css"; // CSS 파일 import
+import "../../css/RecordPage.css";
 
 axios.defaults.baseURL = "http://localhost:8080";
 
@@ -8,19 +8,18 @@ const RecordPage = () => {
     const [records, setRecords] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedRecord, setSelectedRecord] = useState(null);
 
     useEffect(() => {
         const fetchRecords = async () => {
             try {
                 const response = await axios.get("/api/list");
-                console.log(response);
                 setRecords(response.data);
-                console.log(response.data);
             } catch (error) {
                 setError(
                     `시청 기록을 가져오는 데 실패했습니다: ${error.message}`
                 );
-                console.error(error);
             } finally {
                 setLoading(false);
             }
@@ -28,6 +27,16 @@ const RecordPage = () => {
 
         fetchRecords();
     }, []);
+
+    const openModal = (record) => {
+        setSelectedRecord(record);
+        setShowModal(true);
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+        setSelectedRecord(null);
+    };
 
     if (loading) {
         return <div>로딩 중...</div>;
@@ -52,9 +61,28 @@ const RecordPage = () => {
                             <p className="record-title">{record.snippet.title}</p>
                             <p className="record-time">시청시간: {record.watchdata}</p>
                         </div>
+                        <button
+                            className="analysis-button"
+                            onClick={() => openModal(record)}
+                        >
+                            분석 결과
+                        </button>
                     </li>
                 ))}
             </ul>
+
+            {/* 모달 */} 
+            {showModal && (
+                <div className="modal-overlay">
+                    <div className="modal-content">
+                        <h2>분석 결과</h2>
+                        
+                        <button className="close-button" onClick={closeModal}>
+                            닫기
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
