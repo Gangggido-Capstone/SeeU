ae21747db4d5b42670594e89dc408dcbfc25e923
Hanjs89 <hana3269@naver.com>
2024-10-28 01:50:57 +0900

    Store object frequency/order values in the database

--- backend/src/main/java/com/han/youtube/Dto/GazeAnalysisResult.java
+++ backend/src/main/java/com/han/youtube/Dto/GazeAnalysisResult.java
@@ -4,23 +4,30 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Map;
 
 @NoArgsConstructor
 @Getter
 public class GazeAnalysisResult {
-    private List<List<Object>> attentionScoreList;
-    private String gazeVisualization;
+    private List<List<Object>> attentionScoreList; // 비디오 집중 점수
+    private String gazeVisualization; // 비디오 파일 경로
+    private Map<String, Float> objectFrequency; // 객체 빈도수
+    private List<String> objectOrder; // 객체 탐지 순서
 
-    public GazeAnalysisResult(List<List<Object>> attentionScoreList, String gazeVisualization) {
+    public GazeAnalysisResult(List<List<Object>> attentionScoreList, String gazeVisualization, Map<String, Float> objectFrequency, List<String> objectOrder) {
         this.attentionScoreList = attentionScoreList;
         this.gazeVisualization = gazeVisualization;
+        this.objectFrequency = objectFrequency;
+        this.objectOrder = objectOrder;
     }
 
     @Override
     public String toString() {
         return "GazeDataResult{" +
                 "attentionScoreList=" + attentionScoreList +
-                ", gazeVisualization='" + gazeVisualization + '\'' +
+                ", gazeVisualization='" + gazeVisualization +
+                ", objectFrequency='" + objectFrequency +
+                ", objectOrder='" + objectOrder + '\'' +
                 '}';
     }
 }
--- backend/src/main/java/com/han/youtube/Dto/ReceiveIdDto.java
+++ backend/src/main/java/com/han/youtube/Dto/ReceiveIdDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @NoArgsConstructor
 @Getter
@@ -12,24 +13,30 @@ public class ReceiveIdDto {
     private String videoId;
     private String watchdata;
     private LinkedHashMap<String, Object> snippet;
-    private List<List<Object>> scoreList;  // List<List<Object>>로 수정
+    private List<List<Object>> scoreList;
     private String visualization;
+    private Map<String, Float> objectFrequency;
+    private List<String> objectOrder;
 
-    public ReceiveIdDto(String videoId, String watchdata, LinkedHashMap<String, Object> snippet, List<List<Object>> scoreList, String visualization) {
+    public ReceiveIdDto(String videoId, String watchdata, LinkedHashMap<String, Object> snippet, List<List<Object>> scoreList, String visualization, Map<String, Float> objectFrequency, List<String> objectOrder) {
         this.videoId = videoId;
         this.watchdata = watchdata;
         this.snippet = snippet;
         this.scoreList = scoreList;
         this.visualization = visualization;
+        this.objectFrequency = objectFrequency;
+        this.objectOrder = objectOrder;
     }
 
-    public ReceiveId toEntity(String videoId, String watchdata, LinkedHashMap<String, Object> snippet, List<List<Object>> scoreList, String visualization) {
+    public ReceiveId toEntity(String videoId, String watchdata, LinkedHashMap<String, Object> snippet, List<List<Object>> scoreList, String visualization, Map<String, Float> objectFrequency, List<String> objectOrder) {
         return ReceiveId.builder()
                 .videoId(videoId)
                 .watchdata(watchdata)
                 .snippet(snippet)
                 .scoreList(scoreList)
                 .visualization(visualization)
+                .objectFrequency(objectFrequency)
+                .objectOrder(objectOrder)
                 .build();
     }
 }
