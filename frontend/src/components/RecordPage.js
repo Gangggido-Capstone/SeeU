7e091c726dfceccd0f090beea1740df44c008992
Hanjs89 <hana3269@naver.com>
2024-11-01 22:42:25 +0900

    modified width

--- frontend/src/components/RecordPage.js
+++ frontend/src/components/RecordPage.js
@@ -93,7 +93,7 @@ const RecordPage = () => {
                         <h2>분석 결과</h2>
 
                         {selectedRecord.visualization ? (
-                            <video width="320" height="240" controls>
+                            <video width="640" height="480" controls>
                                 <source src={`/data/video/${selectedRecord.visualization}`} type="video/mp4" />
                                 동영상을 지원하지 않는 브라우저입니다.
                             </video>
