a1f0a5eb153e4981077c86c1908e4390cf1c7f3e
Hanjs89 <hana3269@naver.com>
2024-11-02 15:41:30 +0900

    Add null check for gaze data

--- analysis/video_gaze_visualization.py
+++ analysis/video_gaze_visualization.py
@@ -22,77 +22,83 @@ def gazeVisualization(video_id, video_csv, video_only, video_width, video_height
     gaze_csv = pd.read_csv(os.path.join(csv_path, video_csv))
     gaze_csv = gaze_csv.dropna(subset=['Time', 'X', 'Y'])
 
-    points_dir = os.path.join(root_path, "data", "video", video_id, "points")
-    os.makedirs(points_dir, exist_ok=True)
+    print("gaze data: " + str(len(gaze_csv[['X', 'Y']].values)))
+    if(len(gaze_csv[['X', 'Y']].values) <= 10):
+        v = f"{video_id}\\{video_id}.mp4"
+        return v
     
-    video_point = os.path.join(points_dir, f"{video_id}_{date_time}.mp4")
+    else:
+        points_dir = os.path.join(root_path, "data", "video", video_id, "points")
+        os.makedirs(points_dir, exist_ok=True)
+        
+        video_point = os.path.join(points_dir, f"{video_id}_{date_time}.mp4")
 
-    # 비디오 열기
-    cap = cv2.VideoCapture(video_only)
+        # 비디오 열기
+        cap = cv2.VideoCapture(video_only)
 
-    # 비디오 속성 설정
-    fps = cap.get(cv2.CAP_PROP_FPS)  # 초당 프레임 수 가져오기
-    fourcc = cv2.VideoWriter_fourcc(*'avc1')  # 비디오 코덱 설정
-    out = cv2.VideoWriter(video_point, fourcc, fps, (video_width, video_height))  # 비디오 파일 쓰기 설정
+        # 비디오 속성 설정
+        fps = cap.get(cv2.CAP_PROP_FPS)  # 초당 프레임 수 가져오기
+        fourcc = cv2.VideoWriter_fourcc(*'avc1')  # 비디오 코덱 설정
+        out = cv2.VideoWriter(video_point, fourcc, fps, (video_width, video_height))  # 비디오 파일 쓰기 설정
 
-    # DBSCAN 알고리즘을 사용해 클러스터링 수행
-    dbscan = DBSCAN(eps=27, min_samples=5)
-    coords = gaze_csv[['X', 'Y']].values
-    clustering = dbscan.fit(coords)
-    gaze_csv['Cluster'] = clustering.labels_  # 클러스터 라벨 추가
+        # DBSCAN 알고리즘을 사용해 클러스터링 수행
+        dbscan = DBSCAN(eps=27, min_samples=5)
+        coords = gaze_csv[['X', 'Y']].values
+        clustering = dbscan.fit(coords)
+        gaze_csv['Cluster'] = clustering.labels_  # 클러스터 라벨 추가
 
-    # 색상 설정
-    green_color = (0, 255, 0, 128)  # 초록색 (클러스터)
-    red_color = (0, 0, 255, 128)    # 빨간색 (잡음)
+        # 색상 설정
+        green_color = (0, 255, 0, 128)  # 초록색 (클러스터)
+        red_color = (0, 0, 255, 128)    # 빨간색 (잡음)
 
-    # 클러스터 색상 설정: 클러스터가 -1이 아니면 초록색, 아니면 빨간색
-    colors = {
-        label: green_color if label != -1 else red_color
-        for label in np.unique(clustering.labels_)
-    }
+        # 클러스터 색상 설정: 클러스터가 -1이 아니면 초록색, 아니면 빨간색
+        colors = {
+            label: green_color if label != -1 else red_color
+            for label in np.unique(clustering.labels_)
+        }
 
-    points = []  # 화면에 그릴 점 정보 저장
-    point_radius = 7  # 점의 반지름 설정
+        points = []  # 화면에 그릴 점 정보 저장
+        point_radius = 7  # 점의 반지름 설정
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-        frame = cv2.resize(frame, (video_width, video_height))  # 프레임 크기 조정
-        current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0  # 현재 시간 계산
-        frame_points = gaze_csv[(gaze_csv['Time'] >= current_time - 1 / fps) & (gaze_csv['Time'] < current_time)]
+            frame = cv2.resize(frame, (video_width, video_height))  # 프레임 크기 조정
+            current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0  # 현재 시간 계산
+            frame_points = gaze_csv[(gaze_csv['Time'] >= current_time - 1 / fps) & (gaze_csv['Time'] < current_time)]
 
-        overlay = frame.copy()  # 점 그리기를 위한 오버레이 생성
+            overlay = frame.copy()  # 점 그리기를 위한 오버레이 생성
 
-        new_points = []
-        for x, y, t, color in points:
-            if current_time - t <= 1.5:  # 1.5초 이상 지난 점은 제거
-                cv2.circle(overlay, (x, y), point_radius, color[:3], -1)
-                new_points.append((x, y, t, color))
+            new_points = []
+            for x, y, t, color in points:
+                if current_time - t <= 1.5:  # 1.5초 이상 지난 점은 제거
+                    cv2.circle(overlay, (x, y), point_radius, color[:3], -1)
+                    new_points.append((x, y, t, color))
 
-        for _, gaze in frame_points.iterrows():
-            x, y = gaze['X'], gaze['Y']
-            if not pd.isna(x) and not pd.isna(y):
-                x, y = int(x), int(y)
-                cluster_id = gaze['Cluster']
-                color = colors[cluster_id]
-                cv2.circle(overlay, (x, y), point_radius, color[:3], -1)
-                new_points.append((x, y, current_time, color))
+            for _, gaze in frame_points.iterrows():
+                x, y = gaze['X'], gaze['Y']
+                if not pd.isna(x) and not pd.isna(y):
+                    x, y = int(x), int(y)
+                    cluster_id = gaze['Cluster']
+                    color = colors[cluster_id]
+                    cv2.circle(overlay, (x, y), point_radius, color[:3], -1)
+                    new_points.append((x, y, current_time, color))
 
-        # 오버레이와 원본 프레임을 합침
-        alpha = 0.4
-        cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
+            # 오버레이와 원본 프레임을 합침
+            alpha = 0.4
+            cv2.addWeighted(overlay, alpha, frame, 1 - alpha, 0, frame)
 
-        points = new_points
-        out.write(frame)  # 처리된 프레임을 비디오에 저장
+            points = new_points
+            out.write(frame)  # 처리된 프레임을 비디오에 저장
 
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
 
-    point_video = f"{video_id}\\points\\{video_id}_{date_time}.mp4"
-    return point_video
+        point_video = f"{video_id}\\points\\{video_id}_{date_time}.mp4"
+        return point_video
 
 if __name__ == "__main__":
     start = time.time()
